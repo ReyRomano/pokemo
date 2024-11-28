2edb4f3f95bebf203404361a2e0084d5158b743b
Rey Romano <rod.romano.rey@gmail.com>
2024-11-28 16:41:53 -0600

    Mundo poke funcionando

--- src/app/pages/mundo-pokemon/mundo-pokemon.component.ts
+++ src/app/pages/mundo-pokemon/mundo-pokemon.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit,  } from '@angular/core';
 //import { Ability } from 'src/app/interfaces/pokemon-response';
-import { Pokemon } from '../../interfaces/pokemon-response';
+import { Pokemon, Type } from '../../interfaces/pokemon-response';
 import { PokemonsService } from '../../services/pokemons.service';
+import { Ability } from 'src/app/interfaces/pokemon-response';
+import { ActivatedRoute } from '@angular/router';
 
 //  ---------------- Inicio Edito Ok ---------------
-import { catchError, map, of } from 'rxjs';
+import { BusquedaService } from 'src/app/services/busqueda.service';
+import { catchError, delay, map, of } from 'rxjs';
 //  --------------------- Fin edito Ok  -------------------
 
 @Component({
@@ -18,13 +21,35 @@ export class MundoPokemonComponent implements OnInit {
 
   public pokemons: Pokemon[] = [];
 
-  public cargando: boolean = true;
+  public abilitys: Ability[] = [];
 
-  constructor( private PokemonsService: PokemonsService ) {}
+  // public filteredCoins: ICoin[] = [];
+  public filteredPokemons: Pokemon[] = [];
+
+  // --------------------------------------- Inicio Edito Ok:  -------------------->
+  
+  public page: number | undefined;
+  
+  public cargando: boolean = true;
+  
+  // --------------------------------------- Fin Edito Ok:  -------------------->
+
+  constructor( private PokemonsService: PokemonsService, 
+              private Activatedrouter: ActivatedRoute,
+              private busquedaService: BusquedaService ) {}
 
   ngOnInit(): void {
     
     this.cargandoData();
+
+    this.Activatedrouter.params.subscribe( params => {
+    //console.log(params);
+  
+    /*this.pokemonsService.buscarPokemons( params['texto'] ).subscribe( (pokemons: any) => {
+      console.log(pokemons);
+    })*/
+
+    });
 
   }
 
@@ -59,8 +84,47 @@ cargandoData() {
 
       console.log("resultado de API",this.pokemons);
       
+      //this.pokemons=resp.results;
+      //this.abilitys = resp.abilities;
+    
   });
 
 }
+
+//  ---------------------  Inicio Edito Ok:  ------------
+// Buscar Pokemon de mi propio arreglo:
+
+buscarNombre(texto: string ){
+
+  texto = texto.trim();  //Borra espacios delante y detras
+  if (texto.length === 0){  //Si no pone nada, no mandará nada
+    return;
+  }
+  console.log("texto: ", texto);
+
+  // this.coins = this.coins.filter( coin => //: Vas a filtrar
+  this.filteredPokemons = this.pokemons.filter( pokemon => //: Vas a filtrar
+    pokemon.name?.toLowerCase().includes( texto.toLowerCase() )   //:El nombre de pokemon('pokemon.name?') lo vas a convertir primero a minúscula('.toLowerCase()'), y si éste  incluye el texto buscado convertido en minusc también('.includes( this.searchText.toLowerCase())')
+    // coin.symbol?.toLowerCase().includes( this.searchText.toLowerCase() )  //:El símbolo de coin('coin.symbol?') lo vas a convertir primero a minúscula('.toLowerCase()'), y si éste  incluye el texto buscado convertido en minusc también('.includes( this.searchText.toLowerCase())')
+  );
+  console.log("Encontradas: ", this.filteredPokemons );
+  
+}
+//  ---------------------  Fin Edito Ok  ------------
+
+
+/*  buscarHabilidad( texto: string ) {
+
+  texto = texto.trim();  //Borra espacios delante y detras
+  if (texto.length === 0){
+    return;
+  }
+  console.log(texto);
+
+  //this.router.navigate(['/pokemon', texto ]);
+}  */
+
+  // Hacer tabla para pokemonsCapturados
+  
 
 }
